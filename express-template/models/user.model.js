f0ae404019e9016ba4aec4c921de5c9c55615051
maxrangel <lik4ever@hotmail.com>
2022-05-11 19:34:11 -0500

    created template project

---
+++ express-template/models/user.model.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require('sequelize');
+const { db } = require('../utils/database');
+
+const User = db.define('user', {
+  id: {
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+    type: DataTypes.INTEGER,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: 'normal',
+  },
+  status: {
+    type: DataTypes.STRING,
+    defaultValue: 'active',
+  },
+});
+
+module.exports = { User };
