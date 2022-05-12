a35af8202f31b8324366c732d2d322e0330dd2a2
maxrangel <lik4ever@hotmail.com>
2022-05-11 21:53:33 -0500

    completed first deploy

--- heroku-deploy-example/utils/database.js
+++ heroku-deploy-example/utils/database.js
@@ -11,6 +11,15 @@ const db = new Sequelize({
   password: process.env.DB_PASSWORD,
   database: process.env.DB,
   logging: false,
+  dialectOptions:
+    process.env.NODE_ENV === 'production'
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        }
+      : {},
 });
 
 module.exports = { db };
